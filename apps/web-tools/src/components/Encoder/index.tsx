71d4cdadae14fe102286eab575e46957db47d4d5
jongin403 <jongin403@naver.com>
2025-02-15 23:54:06 +0900

    feat: urlConverter 관련 컴포넌트 추가

---
+++ apps/web-tools/src/components/Encoder/index.tsx
@@ -0,0 +1,3 @@
+export const Encoder = () => {
+  return <div>Encoder</div>;
+};
---
+++ apps/web-tools/src/pages/UrlConverter/index.tsx
@@ -0,0 +1,12 @@
+import { Decoder } from '../../components/Decoder';
+import { Encoder } from '../../components/Encoder';
+
+export const UrlConverter = () => {
+  return (
+    <div>
+      UrlConverter
+      <Decoder />
+      <Encoder />
+    </div>
+  );
+};
