319f891797136859cd19561217833726a785c1a4
jongin403 <jongin403@naver.com>
2025-02-22 23:57:04 +0900

    chore: 문구 수정

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,27 +1,27 @@
-import { log } from "@repo/logger";
-import { Link } from "@repo/ui/link";
-import { CounterButton } from "@repo/ui/counter-button";
+import { log } from '@repo/logger';
+import { Link } from '@repo/ui/link';
+import { CounterButton } from '@repo/ui/counter-button';
 
 export const metadata = {
-  title: "Store | Kitchen Sink",
+  title: 'Portfolio | Kitchen Sink',
 };
 
 export default function Store() {
-  log("Hey! This is the Store page.");
+  log('Hey! This is the Portfolio page.');
 
   return (
     <div className="container">
       <h1 className="title">
-        Store <br />
+        Portfolio <br />
         <span>Kitchen Sink</span>
       </h1>
       <CounterButton />
       <p className="description">
-        Built With{" "}
+        Built With{' '}
         <Link href="https://turbo.build/repo" newTab>
           Turborepo
         </Link>
-        {" & "}
+        {' & '}
         <Link href="https://nextjs.org/" newTab>
           Next.js
         </Link>
